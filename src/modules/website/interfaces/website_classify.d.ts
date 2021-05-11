6fa99991a72e5c28958357977e61858c5f9df759
virzs <2369310227@qq.com>
2021-05-11 21:49:23 +0800

    feat 测试接口

---
+++ src/modules/website/interfaces/website_classify.d.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: Vir
+ * @Date: 2021-05-11 10:31:49
+ * @Last Modified by: Vir
+ * @Last Modified time: 2021-05-11 10:32:12
+ */
+
+export interface AddBodyType {
+  name: string;
+  value: string;
+}
--- src/modules/website/services/website_classify.service.ts
+++ src/modules/website/services/website_classify.service.ts
@@ -2,11 +2,12 @@
  * @Author: Vir
  * @Date: 2021-05-10 22:28:16
  * @Last Modified by: Vir
- * @Last Modified time: 2021-05-11 10:07:35
+ * @Last Modified time: 2021-05-11 11:22:31
  */
 
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { AddBodyType } from '../interfaces/website_classify';
 
 @Injectable()
 export class WebsiteClassifyService {
@@ -14,5 +15,13 @@ export class WebsiteClassifyService {
 
   async list() {
     return await this.model.find();
+  }
+
+  async add(body: AddBodyType) {
+    const data = {
+      ...body,
+      create_time: new Date(),
+    };
+    return this.model.create(data);
   }
 }
