80e246cc4c78832ccc2fca6b2e6adebc16aa223d
vir <virs98@outlook.com>
2022-04-21 14:52:02 +0800

    :necktie: 获取 icon key改为环境变量获取

--- src/modules/resource/services/icon.service.ts
+++ src/modules/resource/services/icon.service.ts
@@ -2,7 +2,7 @@
  * @Author: Vir
  * @Date: 2021-05-12 15:15:21
  * @Last Modified by: Vir
- * @Last Modified time: 2021-05-13 22:59:15
+ * @Last Modified time: 2022-04-21 14:50:50
  */
 
 import { HttpService, Injectable } from '@nestjs/common';
@@ -14,7 +14,7 @@ export class IconService {
   async get(query) {
     const response = await this.httpService
       .post(`https://api.webmasterapi.com/v1/favicon`, {
-        apiKey: 'DaiyEDsLDKX3D1bi',
+        apiKey: process.env.webmaster_key,
         url: query.url,
       })
       .toPromise();
